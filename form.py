032f0b6ef3c6f50329b5800da4fd21d02281aa2a
ChairMane <maplestoryilove@gmail.com>
2016-11-06 09:59:46 -0800

    Started adding server functionality

---
+++ form.py
@@ -0,0 +1,12 @@
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField, SubmitField, validators
+
+def CheckNameLength(form, field):
+  if len(field.data) < 4:
+    raise ValidationError('Name must have more then 3 characters')
+
+class ContactForm(Form):
+    name = StringField('Your Name:', [validators.DataRequired(), CheckNameLength])
+    email = StringField('Your e-mail address:', [validators.DataRequired(), validators.Email('your@email.com')])
+    message = TextAreaField('Your message:', [validators.DataRequired()])
+    submit = SubmitField('Send Message')
---
+++ routes.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+from forms import ContactForm
+
+
+app = Flask(__name__)
+app.secret_key = 'YourSuperSecreteKey'
+
+# add mail server config
+app.config['MAIL_SERVER'] = 'smtp.zoho.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'YourUser@NameHere'
+app.config['MAIL_PASSWORD'] = 'yourMailPassword'
+
+mail = Mail(app)
+
+@app.route('/contact', methods=('GET', 'POST'))
+def contact():
+    form = ContactForm()
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return 'Please fill in all fields <p><a href="/contact">Try Again!!!</a></p>'
+        else:
+            msg = Message("Message from your visitor" + form.name.data,
+                          sender='YourUser@NameHere',
+                          recipients=['yourRecieve@mail.com', 'someOther@mail.com'])
+            msg.body = """
+            From: %s <%s>,
+            %s
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+            return "Successfully  sent message!"
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
+
+if __name__ == '__main__':
+    app.run()
